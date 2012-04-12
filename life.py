dce3933426e0f01064d998a8e1c5276b6f329b56
Tomasz Werbicki <tomasz@werbicki.net>
2012-04-12 22:28:23 +0200

    Step 2.

--- life.py
+++ life.py
@@ -14,13 +14,58 @@ class StateParser(object):
 
 class GameOfLife(object):
     def __init__(self, start=None):
-        if not start: start = []
-        self.cells = start
-        self.cells_to_die = []
+        self.cells = start or []
+        self.marked_to_die = []
+        self.marked_to_live = []
+        self.neighbor_counts = {}
 
     def tick(self):
         for cell in self.cells:
             self.process_cell(cell)
+        self.kill_marked_cells()
+        self.spawn_cells()
+
+    def spawn_cells(self):
+        pass
+
+    def kill_marked_cells(self):
+        self.cells = filter(lambda cell: cell not in self.marked_to_die, self.cells)
 
     def process_cell(self, cell):
-        self.cells_to_die.append(cell)
+        if self.should_die(cell):
+            self.marked_to_die.append(cell)
+        else:
+            self.marked_to_live.append(cell)
+        self.increase_neighbors_counts(cell)
+
+    def increase_neighbors_counts(self, cell):
+        neighbors = self.get_neighbors_coordinates(cell)
+        for neighbor in neighbors:
+            if not self.neighbor_counts.get(neighbor):
+                self.neighbor_counts[neighbor] = 0
+            self.neighbor_counts[neighbor] += 1
+
+    def should_die(self, cell):
+        if 1 < self.live_neighbors_count(cell) < 4:
+            return False
+        else:
+            return True
+
+    def live_neighbors_count(self, cell):
+        neighbor_coords = self.get_neighbors_coordinates(cell)
+        count = 0
+        for cell in neighbor_coords:
+            if cell in self.cells:
+                count += 1
+        return count
+
+    def get_neighbors_coordinates(self, cell):
+        neighbors = []
+        for delta_x in range(-1, 2):
+            x = cell[0] + delta_x
+            for delta_y in range(-1, 2):
+                y = cell[1] + delta_y
+                neighbor = (x, y)
+                if not neighbor == cell:
+                    neighbors.append(neighbor)
+        return neighbors
--- test.py
+++ test.py
@@ -15,6 +15,14 @@ class GameOfLifeTest(TestCase):
     def setUp(self):
         self.game = GameOfLife()
 
+    def assertCellMarkedToLive(self, cell):
+        self.assertIn(cell, self.game.marked_to_live)
+        self.assertNotIn(cell, self.game.marked_to_die)
+
+    def assertCellMarkedToDie(self, cell):
+        self.assertIn(cell, self.game.marked_to_die)
+        self.assertNotIn(cell, self.game.marked_to_live)
+
     def test_tick_fires_process_cell_on_each_cell(self):
         self.game.cells = [(1,1), (1,4), (44,54), (124,54)]
         self.game.process_cell = mock.Mock()
@@ -22,7 +30,105 @@ class GameOfLifeTest(TestCase):
         calls = [mock.call(cell) for cell in self.game.cells]
         self.game.process_cell.assert_has_calls(calls)
 
-    def test_process_cell_marks_to_die_if_has_no_neighbors(self):
-        self.game.cells = [(1,1)]
-        self.game.process_cell((1,1))
-        self.assertIn((1,1), self.game.cells_to_die)
+    def test_tick_makes_marked_cells_die(self):
+        self.game.kill_marked_cells = mock.Mock()
+        self.game.tick()
+        self.game.kill_marked_cells.assert_called_once_with()
+
+    def test_tick_spawns_correct_cells(self):
+        self.game.spawn_cells= mock.Mock()
+        self.game.tick()
+        self.game.spawn_cells.assert_called_once_with()
+
+    def test_process_cell_increase_neighbor_counters(self):
+        cell = (1, 1)
+        self.game.cells = [cell]
+        self.game.increase_neighbors_counts = mock.Mock(return_value=True)
+        self.game.process_cell(cell)
+        self.game.increase_neighbors_counts.assert_called_once_with(cell)
+
+    def test_increase_neighbor_counts(self):
+        self.game.neighbor_counts = {
+            (0, 1): 1,
+            (1, 1): 1,
+            (2, 2): 0,
+        }
+        self.game.increase_neighbors_counts((2, 1))
+        expected = {
+            (0, 1): 1,
+            (1, 1): 2,
+            (2, 2): 1,
+        }
+        self.assertDictContainsSubset(expected, self.game.neighbor_counts)
+
+    def test_process_cell_marks_to_die_if_should_die(self):
+        cell = (1, 1)
+        self.game.cells = [cell]
+        self.game.should_die = mock.Mock(return_value=True)
+        self.game.process_cell(cell)
+        self.assertCellMarkedToDie(cell)
+
+    def test_should_die_true_if_lone(self):
+        cell = (1, 1)
+        self.game.cells = [cell]
+        self.assertTrue(self.game.should_die(cell))
+
+    def test_should_die(self):
+        cell = (1, 1)
+        samples = {
+            True: (0, 1, 4, 5, 6, 7, 8),
+            False: (2, 3),
+        }
+
+        self.game.live_neighbors_count = mock.Mock()
+        for expected, counts in samples.items():
+            for c in counts:
+                self.game.live_neighbors_count.return_value = c
+                self.assertEqual(expected, self.game.should_die(cell))
+
+    def test_should_die_true_if_one_neighbor(self):
+        cell = (1, 1)
+        self.game.cells = [cell, (2, 2), (50, 30)]
+        self.game.should_die(cell)
+        self.assertTrue(self.game.should_die(cell))
+
+    def test_should_die_false_if_2_neighbors(self):
+        cell = (1, 1)
+        self.game.cells = [cell, (1, 2), (2, 2), (50, 30)]
+        self.assertFalse(self.game.should_die(cell))
+
+    def test_live_neighbors_count(self):
+        cell = (1, 1)
+        samples = {
+            (cell,): 0,
+            (cell, (2, 2), (50, 30)): 1,
+            (cell, (2, 2), (0, 0), (50, 30)): 2,
+            (cell, (0, 0), (0, 1), (2, 1), (2, 2), (1, 0), (50, 30)): 5,
+        }
+        for sample, expected in samples.items():
+            self.game.cells = sample
+            result = self.game.live_neighbors_count(cell)
+            self.assertEqual(result, expected)
+
+    def test_get_neighbors_coordinates(self):
+        result = self.game.get_neighbors_coordinates((1, 1))
+        expected = [
+            (0, 0),
+            (0, 1),
+            (0, 2),
+            (1, 0),
+            (1, 2),
+            (2, 0),
+            (2, 1),
+            (2, 2),
+        ]
+        self.assertEqual(result, expected)
+
+    def test_kill_marked_cells(self):
+        self.game.cells = [(1, 1), (1, 2), (2, 2), (50, 30)]
+        self.game.marked_to_die = [(1, 1), (2, 2)]
+
+        self.game.kill_marked_cells()
+
+        expected = [(1, 2), (50, 30)]
+        self.assertItemsEqual(self.game.cells, expected)
